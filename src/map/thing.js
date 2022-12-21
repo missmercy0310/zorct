76d878eae57e2cfc5647b4baf327849cff029bf6
missmercy0310 <mistrexxmercy@gmail.com>
2022-12-21 12:08:56 -0800

    Implemented Thing class

---
+++ src/map/thing.js
@@ -0,0 +1,15 @@
+class Thing {
+    constructor(id) {
+        this.id = id;
+        this.takable = false;
+        this.taken = null;
+        this.openable = false;
+        this.open = null;
+        this.readable = false;
+        this.read = null;
+        this.usable = false;
+        this.use = null;
+    }
+}
+
+export default Thing
--- src/map/westOfHouse.js
+++ src/map/westOfHouse.js
@@ -1,34 +1,31 @@
-import Place from "./place"
+import Place from "./place";
+import Thing from "./thing";
 
 const westOfHouse = new Place("westOfHouse");
 westOfHouse.name = "West of House";
 westOfHouse.description = "You are standing in an open field west of a white house, with a boarded front door. There is a small mailbox here.";
-westOfHouse.things = [{
-    id: "letter",
-    takable: true,
-    taken: false,
-    openable: false,
-    opened: null,
-    readable: true,
-    read: <div className="response letter">
-        <div><p>The letter reads as follows:</p></div>
-        <div>
-            <p>Welcome to Hell!</p>
-            <p>You have been sent here for misuse of the word "multitudinous." Have a look around!</p>
-            <p>Yours truly,</p>
-            <p>P.Code</p>
-        </div>
-        <div><p>This concludes the letter.</p></div>
+
+const letter = new Thing("letter");
+letter.takable = true;
+letter.taken = false;
+letter.readable = true;
+letter.read = <div className="response letter">
+    <div><p>The letter reads as follows:</p></div>
+    <div>
+        <p>Welcome to Hell!</p>
+        <p>You have been sent here for misuse of the word "multitudinous." Have a look around!</p>
+        <p>Yours truly,</p>
+        <p>P.Code</p>
     </div>
-},
-{
-    id: "mailbox",
-    takable: false,
-    taken: null,
-    openable: true,
-    opened: <div className="response"><p>There is a letter in the mailbox.</p></div>,
-    readable: true,
-    read: <div className="response"><p>The mailbox is labeled. The label reads "666 Hell St."</p></div>
-}];
+    <div><p>This concludes the letter.</p></div>
+</div>;
+
+const mailbox = new Thing("mailbox");
+mailbox.openable = true;
+mailbox.opened = <div className="response"><p>There is a letter in the mailbox.</p></div>;
+mailbox.readable = true;
+mailbox.read = <div className="response"><p>The mailbox is labeled. The label reads "666 Hell St."</p></div>;
+
+westOfHouse.things = [letter, mailbox];
 
 export default westOfHouse
